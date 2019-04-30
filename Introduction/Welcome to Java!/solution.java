e85207cb28e18d5344eeb650a5cf29028a85f757
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-04-30 12:05:22 -0400

    Update solution.java

--- Introduction/Welcome to Java!/solution.java
+++ Introduction/Welcome to Java!/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
 
     public static void main(String[] args) {
+        // Solution
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
     }
